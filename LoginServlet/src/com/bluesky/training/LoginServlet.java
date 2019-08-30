3767c1237593fd5611909804e6c5722e4f525efa
premkama98 <kamalosipalli@gmail.com>
2019-08-30 17:42:14 +0530

    second commit

--- LoginServlet/src/com/bluesky/training/LoginServlet.java
+++ LoginServlet/src/com/bluesky/training/LoginServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Loginservlet")
-public class Loginservlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    
